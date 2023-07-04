a4eb50e6b86ab7d122a28f32f0187e52b778b351
pavankborra <71217912+pavankborra@users.noreply.github.com>
2023-07-04 12:12:36 +0530

    added new code to test calling weather API in the apicall.py file

--- apicall.py
+++ apicall.py
@@ -1,43 +1,27 @@
-from urllib.request import urlopen
-from html.parser import HTMLParser
+import requests
+import os
 
-class WeatherParser(HTMLParser):
-    def _init_(self):
-        super()._init_()
-        self.in_location = False
-        self.in_temperature = False
-        self.in_description = False
-        self.location = ""
-        self.temperature = ""
-        self.description = ""
+BASE_URL = "https://api.zippopotam.us/us/"
 
-    def handle_starttag(self, tag, attrs):
-        attrs = dict(attrs)
-        if tag == "span" and "class" in attrs:
-            classes = attrs["class"].split()
-            self.in_location = "wr-c-location__name" in classes
-            self.in_temperature = "wr-value--temperature--c" in classes
-            self.in_description = "wr-c-weather-type-description" in classes
+# zipcode = input("Enter a zipcode: ")
+zipcode = "66614"
+requests_url = f"{BASE_URL}{zipcode}"
+response = requests.get(requests_url)
 
-    def handle_data(self, data):
-        if self.in_location:
-            self.location = data.strip()
-            self.in_location = False
-        elif self.in_temperature:
-            self.temperature = data.strip()
-            self.in_temperature = False
-        elif self.in_description:
-            self.description = data.strip()
-            self.in_description = False
+# This can be used to test the api call or uncomment this to see how the info below gets pulled from the API
+# data = response.json()
+# print(data)
 
-def get_weather(location):
-    with urlopen(f"https://www.bbc.co.uk/weather/{location}") as response:
-        parser = WeatherParser()
-        parser.feed(response.read().decode())
-        print(f"Weather in {parser.location}: {parser.description}, Temperature: {parser.temperature}")
-
-def main():
-    get_weather('2147714')  # Dubai's location ID on BBC Weather
-
-if _name_ == "_main_":
-    main()
+if response.status_code == 200:
+    data = response.json()
+    country = data['country']
+    place_name = data['places'][0]['place name']    
+    state_name = data['places'][0]['state']    
+    state_code = data['places'][0]['state abbreviation']  
+    
+    print("Country:", country)
+    print("State:", state_name)
+    print("State Code:", state_code)
+    print("Place name:", place_name)
+else:
+    print("An error occurred.")
